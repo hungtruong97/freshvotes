e5c2dcdf2289a6a9375807aae4f183fda8c58079
Hung Truong <hungtruong97@gmail.com>
2020-08-26 20:44:00 +0700

    update login page

--- src/main/java/com/freshvotes/demo/web/LoginController.java
+++ src/main/java/com/freshvotes/demo/web/LoginController.java
@@ -1,13 +1,14 @@
-package com.freshvotes.demo;
+package com.freshvotes.demo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class DashboardController {
-    @RequestMapping(value="/", method=RequestMethod.GET)
-    public String rootView() {
-        return "index";
+public class LoginController {
+
+    @RequestMapping(value="/login", method= RequestMethod.GET)
+    public String login () {
+        return "login";
     }
 }
