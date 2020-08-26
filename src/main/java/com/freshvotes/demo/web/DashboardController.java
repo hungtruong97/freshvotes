832c64e5946b983763f3e371bda2b1c0adc35184
Hung Truong <hungtruong97@gmail.com>
2020-08-26 21:32:57 +0700

    update password encoder and dashboard.html

--- src/main/java/com/freshvotes/demo/web/DashboardController.java
+++ src/main/java/com/freshvotes/demo/web/DashboardController.java
@@ -1,13 +1,21 @@
 package com.freshvotes.demo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DashboardController {
-    @RequestMapping(value="/", method=RequestMethod.GET)
+    @GetMapping("/")
     public String rootView() {
         return "index";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
     }
 }
